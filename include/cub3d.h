8247bfd3a6d1e11ff0153bb6e5be9535fc9dedca
Jan-arvid Harrach <jharrach@1-C-7.42heilbronn.de>
2023-03-24 20:45:52 +0100

    dda (some edge cases don't work)

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: jharrach <jharrach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 14:29:48 by jharrach          #+#    #+#             */
-/*   Updated: 2023/03/20 15:17:58 by jharrach         ###   ########.fr       */
+/*   Updated: 2023/03/24 18:14:00 by jharrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,56 @@
 # define CUB3D_H
 
 # include <stdio.h>
+# include <math.h>
 # include "../libft/include/libft.h"
 # include "../mlx42/include/MLX42/MLX42.h"
+
+# define WIDTH 1600
+# define HEIGHT 900
+# define FOV 90.0f
+# define PI 3.141592653589793
+
+typedef struct	s_vec2f
+{
+	float	x;
+	float	y;
+}	t_vec2f;
+
+typedef struct	s_vec2i
+{
+	int	x;
+	int	y;
+}	t_vec2i;
+
+/**
+ * @param texture Wall textures
+ * @param pos Player position
+ * @param map
+ * @param mlx
+ * @param win Main image
+ * @param delta_time
+ * @param dir Player direction in radians
+ * @param fov Filed of view in radians
+ * @param dis Distance from pov to window
+ * @param col_floor
+ * @param col_ceiling
+ * @param win_wh Half width of main image
+**/
+
+typedef struct	s_data
+{
+	mlx_image_t	texture[4];
+	t_vec2f		pos;
+	int			**map;
+	mlx_t		*mlx;
+	mlx_image_t	*win;
+	double		delta_time;
+	float		dir;
+	float		fov;
+	float		dis;
+	uint32_t	col_floor;
+	uint32_t	col_ceiling;
+	uint32_t	win_wh;
+}	t_data;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -2,17 +2,140 @@
 
 void ft_hook(void* param)
 {
-	mlx_t *const mlx = param;
+	t_data *const	data = param;
+	int32_t		i;
+	int				j;
+	int				k;
+	float			a;
+	float			del_a;
+	t_vec2f			dir;
+	t_vec2f			unit;
+	t_vec2i			step;
+	t_vec2f			delta;
+	t_vec2i			pos;
+	t_vec2f			leng;
+	float			len;
 
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
+	ft_memset(data->win->pixels, 0, data->win->width * data->win->height * 4);
+	i = 0;
+	while (i < data->mlx->width)
+	{
+		del_a = atanf((float)(i - (int)data->win_wh) / data->dis);
+		a = data->dir + del_a;
+		dir = (t_vec2f){.x = -sinf(a), .y = cosf(a)};
+		if (dir.x >= 0.0)
+		{
+			step.x = 1;
+			delta.y = 1.0 - (data->pos.x - (int)data->pos.x);
+		}
+		else
+		{
+			step.x = -1;
+			delta.y = (data->pos.x - (int)data->pos.x);
+		}
+		if (dir.y >= 0.0)
+		{
+			step.y = 1;
+			delta.x = 1.0 - (data->pos.y - (int)data->pos.y);
+		}
+		else
+		{
+			step.y = -1;
+			delta.x = 1.0 - (data->pos.y - (int)data->pos.y);
+		}
+		if (a != 0.0)
+		{
+			unit.y = fabsf(1.0f / sinf(a));
+			leng.y = unit.y * delta.y;
+		}
+		// else
+		// {
+		// 	unit.y = 50.0;
+		// 	leng.y = unit.y * delta.y;
+		// }
+		if (a != 90.0)
+		{
+			unit.x = fabsf(1.0f / cosf(a));
+			leng.x = unit.x * delta.x;
+		}
+		// else
+		// {
+		// 	unit.x = 50.0;
+		// 	leng.x = unit.x * delta.x;
+		// }
+		pos = (t_vec2i){.x = data->pos.x, .y = data->pos.y};
+		while (1)
+		{
+			if (a != 0 && leng.y < leng.x)
+			{
+				len = leng.y;
+				pos.x += step.x;
+				leng.y += unit.y;
+			}
+			else if (a != 90.0)
+			{
+				len = leng.x;
+				pos.y += step.y;
+				leng.x += unit.x;
+			}
+			if (len > 50.0)
+				break ;
+			if (pos.x >= 0 && pos.x < 6 && pos.y >= 0 && pos.y < 5 && data->map[pos.x][pos.y])
+			{
+				len = data->dis / (len * cosf(del_a));
+				j = data->mlx->height / 2.0 - len / 2.0;
+				k = 0;
+				while (k++ < len)
+				{
+					if (j >= 0 && j < data->mlx->height)
+						mlx_put_pixel(data->win, i, j, 0xFFFFFFFF);
+					j++;
+				}
+				break ;
+			}
+		}
+		i++;
+	}
+
+	if (mlx_is_key_down(data->mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(data->mlx);
+	if (mlx_is_key_down(data->mlx, MLX_KEY_W))
+		data->pos.y += 0.01;
+	if (mlx_is_key_down(data->mlx, MLX_KEY_S))
+		data->pos.y -= 0.01;
+	if (mlx_is_key_down(data->mlx, MLX_KEY_A))
+		data->pos.x += 0.01;
+	if (mlx_is_key_down(data->mlx, MLX_KEY_D))
+		data->pos.x -= 0.01;
+
+	if (mlx_is_key_down(data->mlx, MLX_KEY_RIGHT))
+		data->dir += 0.01;
+	if (mlx_is_key_down(data->mlx, MLX_KEY_LEFT))
+		data->dir -= 0.01;
 }
 
 int	main(void)
 {
-	mlx_t *const mlx = mlx_init(500, 500, "cub3D", 0);
-	ft_printf("libft\n");
-	mlx_loop_hook(mlx, ft_hook, mlx);
+	t_data	data;
+	mlx_t	*mlx;
+
+	data.map = malloc(sizeof(*(data.map)) * 6);
+	data.map[0] = (int []){1, 1, 1, 1, 1};
+	data.map[1] = (int []){1, 0, 0, 0, 1};
+	data.map[2] = (int []){1, 0, 0, 0, 1};
+	data.map[3] = (int []){1, 0, 0, 0, 1};
+	data.map[4] = (int []){1, 0, 0, 0, 1};
+	data.map[5] = (int []){1, 1, 1, 1, 1};
+	data.fov = FOV * PI / 180.0;
+	data.pos = (t_vec2f){.x = 2.0f, .y = 2.0f};
+	data.dir = 0.0f;
+	mlx = mlx_init(WIDTH, HEIGHT, "cub3D", 0);
+	data.win_wh = mlx->width / 2;
+	data.dis = (float)data.win_wh / tanf(data.fov / 2.0);
+	data.mlx = mlx;
+	data.win = mlx_new_image(mlx, mlx->width, mlx->height);
+	mlx_image_to_window(mlx, data.win, 0, 0);
+	mlx_loop_hook(mlx, ft_hook, &data);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 }
